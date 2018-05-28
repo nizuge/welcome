21570a970abec5698ca450fda7cec8489b923c35
anytec-z <anytec-z@z.com>
2018-05-28 16:47:26 +0800

    debug

--- src/main/java/cn/anytec/welcome/quadrant/findface/FindFaceRunnable.java
+++ src/main/java/cn/anytec/welcome/quadrant/findface/FindFaceRunnable.java
@@ -7,6 +7,7 @@ import cn.anytec.welcome.quadrant.pojo.FDCameraData;
 import cn.anytec.welcome.quadrant.pojo.IdentifyPojo;
 import cn.anytec.welcome.util.HttpUtil;
 import cn.anytec.welcome.util.ImageUtil;
+import com.alibaba.fastjson.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +15,7 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.ArrayDeque;
-import java.util.Base64;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.Semaphore;
 
 
@@ -51,11 +50,14 @@ public class FindFaceRunnable implements Runnable{
             identifyPojo.getResults().keySet().forEach((key)->{
                 try {
                     List<IdentifyPojo.MatchFace> matchFaces = identifyPojo.getResults().get(key);
+                    Map<String,Object> replyInfo = new HashMap<>();
                     if(null != matchFaces && matchFaces.size() > 0){
                         IdentifyPojo.MatchFace matchFace= matchFaces.get(0);
                         if(matchFace.getConfidence() > config.getSdk_api_identify_threshold()){
                             byte[] identified = HttpUtil.getBinaryDataByURL(matchFace.getFace().getPhoto());
-                            simpMessagingTemplate.convertAndSend("/topic/"+data.mStrMac,"{\"identified\":\""+Base64.getEncoder().encodeToString(identified)+"\"}");
+                            replyInfo.put("identified",Base64.getEncoder().encodeToString(identified));
+                            replyInfo.put("meta",matchFace.getFace().getMeta());
+                            simpMessagingTemplate.convertAndSend("/topic/"+data.mStrMac,new JSONObject(replyInfo).toJSONString());
                         }
                     }else {
                         String[] coordinate = key.substring(1,key.length()-1).replace(" ","").split(",");
@@ -63,10 +65,11 @@ public class FindFaceRunnable implements Runnable{
                         int y1 = Integer.parseInt(coordinate[1]);
                         int x2 = Integer.parseInt(coordinate[2]);
                         int y2 = Integer.parseInt(coordinate[3]);
-                        x1 = x1 < 0 ? 0: x1;
-                        y1 = y1 < 0 ? 0: y1;
+                        x1 = x1 < 0 ? 0 : x1;
+                        y1 = y1 < 0 ? 0 : y1;
                         byte[] unrecognized = ImageUtil.cutImg(data.mJpgData,"jpeg",x1,y1,x2-x1,y2-y1);
-                        simpMessagingTemplate.convertAndSend("/topic/"+data.mStrMac,"{\"unrecognized\":\""+ Base64.getEncoder().encodeToString(unrecognized)+"\"}");
+                        replyInfo.put("unrecognized",Base64.getEncoder().encodeToString(unrecognized));
+                        simpMessagingTemplate.convertAndSend("/topic/"+data.mStrMac,new JSONObject(replyInfo).toJSONString());
                     }
 
                 } catch (IOException e) {
